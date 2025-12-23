aba78fb0d7ae74ae2f9e03eecbda527c82e6d01b
Sude Kirlar <sudekrlr@gmail.com>
2025-12-23 05:15:02 +0300

    ci: load-test fix (external js problem)

--- k6/load-test.js
+++ k6/load-test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js";
 
 export const options = {
     stages: [
